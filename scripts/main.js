061f5487ee1fcb775b5e64b5a65d78b414dae6cd
unknown <gamerxokks@gmail.com>
2022-02-25 09:15:54 +0300

    Задача генерация array

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,12 @@
 function calc()
 {
-  var n,s;
-  n = document.getElementById('num').value;
-  s = document.getElementById('result');
-  n = parseInt(n);
-
-  if (n < 10) {
-	s.innerHTML = 'Вы жеребенок';
-	console.log('Вы жеребенок');
-  } 
-  else if (n < 17) {
-    s.innerHTML = 'Подросток';
-	console.log('Подросток');
-  }
-  else if (n > 59) {
-    s.innerHTML = 'Вы пожилой';
-	console.log('Вы пожилой');
-  }
-  else {
-	s.innerHTML = 'Вы взрослый';
-	console.log('Вы взрослый');
-  }
+	var s;
+	let arr = []
+	const n = 10
+	for (let i=0;i<n; i++) {
+		arr[i]=Math.floor(Math.random()*10)
+	}
+    s = document.getElementById('result');
+	console.log(arr)
+	s.innerHTML = arr;
 }
