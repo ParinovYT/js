d1933914453b413f196137778d762cff62b6a443
unknown <gamerxokks@gmail.com>
2022-03-16 07:04:30 +0300

    23) Рисование с помощью JS heart

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,14 @@ function draw_filled_circle()
 	var ctx = canvas.getContext('2d');
 	
 	ctx.beginPath();
-	ctx.moveTo(20,20);
-	ctx.bezierCurveTo(30, 147, 108, 145, 130, 48);
-	ctx.stroke();
-	
-	ctx.beginPath();
-	ctx.lineWidth = 5; 
-	ctx.strokeStyle="green";
-	ctx.moveTo(100, 150); 
-	ctx.bezierCurveTo(150, 50, 340, 10, 400, 200);
-	ctx.stroke(); 
-	
+	ctx.fillStyle="red";
+	ctx.moveTo(75, 40);
+	ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+	ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+	ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+	ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+	ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+	ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+	ctx.fill();    
 	
 }
