c4ffd5bf1f70e254a4a4c5ecc4f427eab709eb4a
unknown <gamerxokks@gmail.com>
2022-03-16 06:53:44 +0300

    5) Пример работы с объектами 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,36 @@
-let x = { 'name':'Rustam', 'age':18, 'height':160.5 }
-
-function svLog(k)
-{
-	var s = document.getElementById('result')
-	console.log(k)
-	s.innerHTML = k;
+let sv_current = 0x0
+let students = [
+	{'id':1, 'name':'Danila','surname':'Alferov'},
+	{'id':2, 'name':'Tihon','surname':'Balaganski'},
+	{'id':3, 'name':'Konstantin','surname':'Bobrovski'},
+	{'id':4, 'name':'Vladimir','surname':'Burikin'},
+	{'id':5, 'name':'Semen','surname':'Veligjanin'}
+]
+function load_student(id) {
+	document.getElementById('name').value = students[id].name
+	document.getElementById('surname').value = students[id].surname
 }
-
-function calc()
-{
-	svLog('Name: '+x.name)
+function next() {
+	document.getElementById('m_iBack').disabled = false
+	sv_current++
+	load_student(sv_current)
+	if (sv_current == 0x4) {
+		document.getElementById('m_iNext').disabled = true
+	}
 }
-
+function back() {
+	document.getElementById('m_iNext').disabled = false
+	sv_current--
+	load_student(sv_current)
+	console.log(sv_current)
+	if (sv_current == 0x0) {
+		document.getElementById('m_iBack').disabled = true
+	}
+}
+function get_all_to_console()
+{
+	console.clear()
+	for (let i=0; i<students.length; i++) {
+		console.log(students[i].surname+' '+students[i].name)
+	}
+}
