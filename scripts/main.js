e333badbfcec64d822a1e0eacef7a8f176d2920f
unknown <gamerxokks@gmail.com>
2022-03-16 06:55:36 +0300

    10) Рисование с помощью JS + обводка

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 function draw_fill() {
 	let _canvas = document.getElementById('g_render')
 	let ctx = _canvas.getContext('2d')
-	ctx.fillStyle = 'green'
-	ctx.fillRect(0, 0, 400, 400); //args: x,y,w,h
+	ctx.strokeStyle = "green"; //цвет обводки
+	ctx.lineWidth = "10"; //толщина обводки
+	ctx.rect(10, 10, 380, 380); //args: x,y,w,h
+	ctx.stroke();
+	ctx.fillStyle = "black"; //цвет заливки
+	ctx.fill();  
 }
