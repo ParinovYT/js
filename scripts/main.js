95913c1f480ab50a55d23689c78b18abd3dbd301
unknown <gamerxokks@gmail.com>
2022-03-16 07:05:56 +0300

    25) Рисование с помощью JS anim zoom

--- scripts/main.js
+++ scripts/main.js
@@ -3,16 +3,16 @@ function start_anim()
 var canvas=document.getElementById('g_render');
 var ctx = canvas.getContext('2d');
 
-let pox_X = 75; 
+let size = 75; 
 setInterval(function() {
     ctx.beginPath();
     ctx.clearRect(0,0,400,400); 
     ctx.fillStyle = "green"; 
-    ctx.rect(pox_X, 50, 50, 50);
+    ctx.rect(75, 50, size, size);
     ctx.fill();
-    pox_X++;
-    if(pox_X > 600) {
-        pox_X = 50;
+    size++;
+    if(size > 350) {
+        size = 50;
     }
 },1); 
 }
