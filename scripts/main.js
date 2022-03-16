2389c1887689d07aa012cb4d28a01db65b54a3bd
unknown <gamerxokks@gmail.com>
2022-03-16 07:02:30 +0300

    20) Рисование с помощью JS полная окружность

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,14 @@
-function draw_arc() 
+function draw_filled_circle() 
 {
 	var canvas=document.getElementById('g_render');
 	var ctx = canvas.getContext('2d');
 	
 	ctx.beginPath();
-	ctx.lineWidth = 5; 
-	ctx.arc(200,200,100,0, Math.PI,true);
+	ctx.lineWidth = 15;
+	ctx.strokeStyle = "orange";
+	ctx.fillStyle="blue";
+	ctx.arc(200, 200, 90, 0, 2*Math.PI, false);
 	ctx.stroke();
-	
-	ctx.beginPath();
-	ctx.lineWidth = 5; 
-	ctx.arc(200,200 + 10,100,0, Math.PI,false);
-	ctx.stroke();
+	ctx.fill(); 
 	
 }
