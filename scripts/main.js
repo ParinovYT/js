4e50b199b2aeaa957183d93d1784d51b1d101bc0
unknown <gamerxokks@gmail.com>
2022-03-16 07:01:07 +0300

    18) Рисование с помощью JS очеред пример 1

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,17 @@ function draw_line() {
 	let _canvas = document.getElementById('g_render')
 	let ctx = _canvas.getContext('2d')
 
-	ctx.beginPath();
-	ctx.strokeStyle = "red";
-	ctx.lineWidth = "5";
-	ctx.moveTo(100,150);
-	ctx.lineTo(200,200);
-	ctx.stroke();
+	let x = 300
+	let y = 80
+	let z = 70
 
 	ctx.beginPath();
-	ctx.strokeStyle = "green";
-	ctx.lineWidth = "5";
-	ctx.moveTo(200,50);
-	ctx.lineTo(150,150);
+	ctx.strokeStyle = "red";
+	ctx.lineWidth = "10";
+	ctx.moveTo(z,x);
+	ctx.lineTo(x,50);
+	ctx.lineCap = "round";
+	ctx.lineTo(400,x);
+	ctx.lineTo(y,x);
 	ctx.stroke(); 
 }
