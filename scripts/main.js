e265ae3bfb1cac5e22e44f97c40abac2d1120c2d
unknown <gamerxokks@gmail.com>
2022-03-16 06:55:11 +0300

    9) Рисование с помощью JS + color

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 function draw_fill() {
 	let _canvas = document.getElementById('g_render')
 	let ctx = _canvas.getContext('2d')
+	ctx.fillStyle = 'green'
 	ctx.fillRect(0, 0, 400, 400); //args: x,y,w,h
 }
