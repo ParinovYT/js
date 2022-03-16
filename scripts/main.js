79010433f51f2564bd5c688e7bbd457e7755b85b
unknown <gamerxokks@gmail.com>
2022-03-16 06:59:28 +0300

    15) Рисование с помощью JS просто линии 2е

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,12 @@ function draw_line() {
 	let _canvas = document.getElementById('g_render')
 	let ctx = _canvas.getContext('2d')
 
-	ctx.beginPath();
-	ctx.strokeStyle = "red";
-	ctx.lineCap = "round";
-	ctx.lineWidth = "50";
-	ctx.moveTo(350,50);
-	ctx.lineTo(50,350);
-	ctx.stroke();   
+	ctx.moveTo(0,400);
+	ctx.lineTo(400,0);
+	ctx.strokeStyle = "green"; 
+	ctx.lineWidth = "5"; 
+	ctx.stroke(); 
+	ctx.moveTo(0,0);
+	ctx.lineTo(400,400);
+	ctx.stroke();    
 }
